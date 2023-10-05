9578b090a54a4ac0424e329a04d82423fe8685ba
davidagina <davidagina64@gmail.com>
2023-10-05 21:31:35 +0100

    updated

--- src/main/java/com/daveproject/todo/controller/todoController.java
+++
@@ -1,7 +0,0 @@
-package com.daveproject.todo.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class todoController {
-}
--- src/main/java/com/daveproject/todo/repository/todoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.daveproject.todo.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class todoRepository {
-}
