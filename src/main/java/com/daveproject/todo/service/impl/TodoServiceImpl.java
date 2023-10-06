943108c70b9c3f20139c68e91f95957f63e56d0f
davidagina <davidagina64@gmail.com>
2023-10-06 09:52:45 +0100

    Added Todo ServiceImpl class

---
+++ src/main/java/com/daveproject/todo/service/impl/TodoServiceImpl.java
@@ -0,0 +1,37 @@
+package com.daveproject.todo.service.impl;
+
+import com.daveproject.todo.dto.TodoDto;
+import com.daveproject.todo.entity.Todo;
+import com.daveproject.todo.repository.TodoRepository;
+import com.daveproject.todo.service.TodoService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TodoServiceImpl implements TodoService {
+
+    private TodoRepository todoRepository;
+
+    @Override
+    public TodoDto addTodo(TodoDto todoDto){
+
+        // Convert TodoDto into Todo Jpa entity
+        Todo todo = new Todo();
+        todo.setTitle(todoDto.getTitle());
+        todo.setDescription(todoDto.getDescription());
+        todo.setCompleted(todoDto.isCompleted());
+
+        // Todo jpa entity
+        Todo savedTodo = todoRepository.save(todo);
+
+        //  convert saved Todo Jpa entity object into TodoDto object
+        TodoDto savedTodoDto = new TodoDto();
+        savedTodoDto.setId(savedTodo.getId());
+        savedTodoDto.setTitle(savedTodoDto.getTitle());
+        savedTodoDto.setDescription(savedTodoDto.getDescription());
+        savedTodoDto.setCompleted(savedTodoDto.isCompleted());
+
+        return savedTodoDto;
+    }
+}
