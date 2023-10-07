41d8871850112f96142d6836b920673a8883dbdb
davidagina <davidagina64@gmail.com>
2023-10-07 08:12:53 +0100

    Updated

--- src/main/java/com/daveproject/todo/controller/TodoController.java
+++ src/main/java/com/daveproject/todo/controller/TodoController.java
@@ -5,6 +5,8 @@ import com.daveproject.todo.service.TodoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +18,9 @@ public class TodoController {
     private TodoService todoService;
 
     // Build Add Todo REST API
-    public ResponseEntity<TodoDto> addTodo(TodoDto todoDto){
+
+    @PostMapping
+    public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto){
 
         TodoDto savedTodo = todoService.addTodo(todoDto);
         return new ResponseEntity<>(savedTodo, HttpStatus.CREATED);
