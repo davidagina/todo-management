0ad93f6dd67ecc56662738aa0c167a7f5d87bc8d
davidagina <davidagina64@gmail.com>
2023-10-08 22:39:13 +0100

    Added GetTodo method

--- src/main/java/com/daveproject/todo/controller/TodoController.java
+++ src/main/java/com/daveproject/todo/controller/TodoController.java
@@ -5,10 +5,7 @@ import com.daveproject.todo.service.TodoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/todos")
@@ -24,6 +21,13 @@ public class TodoController {
 
         TodoDto savedTodo = todoService.addTodo(todoDto);
         return new ResponseEntity<>(savedTodo, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public  ResponseEntity<TodoDto> getTodo(@PathVariable("id") Long todoId){
+        TodoDto todoDto = todoService.getTodo(todoId);
+        return ResponseEntity.ok(todoDto);
+//        return new ResponseEntity<>(todoDto, HttpStatus.OK);
     }
 
 }
