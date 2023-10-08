c94dac9e9c5be4bcc2daf831978909478f882c3b
davidagina <davidagina64@gmail.com>
2023-10-08 04:57:40 +0100

    Implemented Model Mapper

--- src/main/java/com/daveproject/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/com/daveproject/todo/service/impl/TodoServiceImpl.java
@@ -5,6 +5,7 @@ import com.daveproject.todo.entity.Todo;
 import com.daveproject.todo.repository.TodoRepository;
 import com.daveproject.todo.service.TodoService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,25 +13,30 @@ import org.springframework.stereotype.Service;
 public class TodoServiceImpl implements TodoService {
 
     private TodoRepository todoRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public TodoDto addTodo(TodoDto todoDto){
 
         // Convert TodoDto into Todo Jpa entity
-        Todo todo = new Todo();
-        todo.setTitle(todoDto.getTitle());
-        todo.setDescription(todoDto.getDescription());
-        todo.setCompleted(todoDto.isCompleted());
+//        Todo todo = new Todo();
+//        todo.setTitle(todoDto.getTitle());
+//        todo.setDescription(todoDto.getDescription());
+//        todo.setCompleted(todoDto.isCompleted());
+
+        Todo todo = modelMapper.map(todoDto, Todo.class);
 
         // Todo jpa entity
         Todo savedTodo = todoRepository.save(todo);
 
         //  convert saved Todo Jpa entity object into TodoDto object
-        TodoDto savedTodoDto = new TodoDto();
-        savedTodoDto.setId(savedTodo.getId());
-        savedTodoDto.setTitle(savedTodo.getTitle());
-        savedTodoDto.setDescription(savedTodo.getDescription());
-        savedTodoDto.setCompleted(savedTodo.isCompleted());
+//        TodoDto savedTodoDto = new TodoDto();
+//        savedTodoDto.setId(savedTodo.getId());
+//        savedTodoDto.setTitle(savedTodo.getTitle());
+//        savedTodoDto.setDescription(savedTodo.getDescription());
+//        savedTodoDto.setCompleted(savedTodo.isCompleted());
+
+        TodoDto savedTodoDto = modelMapper.map(savedTodo, TodoDto.class);
 
         return savedTodoDto;
     }
