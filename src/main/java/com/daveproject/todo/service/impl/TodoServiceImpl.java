e01541eae582a725193aa3f60974f6fde861ffd7
davidagina <davidagina64@gmail.com>
2023-10-09 23:54:17 +0100

    Added getAllTodos method

--- src/main/java/com/daveproject/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/com/daveproject/todo/service/impl/TodoServiceImpl.java
@@ -9,6 +9,9 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class TodoServiceImpl implements TodoService {
@@ -50,5 +53,14 @@ public class TodoServiceImpl implements TodoService {
 
 
         return modelMapper.map(todo, TodoDto.class);
+    }
+
+    @Override
+    public List<TodoDto> getAllTodos() {
+
+        List<Todo> todos = todoRepository.findAll();
+
+        return todos.stream().map((todo) -> modelMapper.map(todo, TodoDto.class))
+                .collect(Collectors.toList());
     }
 }
