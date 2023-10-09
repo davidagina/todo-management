0781bd35e8f23b5b7ad6e71a768c8574c1a973f6
davidagina <davidagina64@gmail.com>
2023-10-09 23:54:12 +0100

    Added getAllTodos method

--- src/main/java/com/daveproject/todo/service/TodoService.java
+++ src/main/java/com/daveproject/todo/service/TodoService.java
@@ -2,9 +2,13 @@ package com.daveproject.todo.service;
 
 import com.daveproject.todo.dto.TodoDto;
 
+import java.util.List;
+
 public interface TodoService {
 
     TodoDto addTodo(TodoDto todoDto);
 
     TodoDto getTodo(Long id);
+
+    List<TodoDto> getAllTodos();
 }
