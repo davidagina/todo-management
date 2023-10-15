934437d8e2fcd7f9deaee00397fc0bc8a757b09c
davidagina <davidagina64@gmail.com>
2023-10-15 16:05:19 +0100

    Added roles

--- src/main/java/com/daveproject/todo/config/SpringSecurityConfig.java
+++ src/main/java/com/daveproject/todo/config/SpringSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +21,23 @@ public class SpringSecurityConfig {
                     authorize.anyRequest().authenticated();
         }).httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    public UserDetailsService userDetailsService(){
+
+        UserDetails ramesh = User.builder()
+                .username("dave")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(ramesh, admin);
     }
 
 }
