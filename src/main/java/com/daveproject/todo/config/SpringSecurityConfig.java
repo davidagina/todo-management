a99bba5d3c59093b7c2568c919bd5171d98566ae
davidagina <davidagina64@gmail.com>
2023-10-15 16:07:20 +0100

    Added roles and password encoder

--- src/main/java/com/daveproject/todo/config/SpringSecurityConfig.java
+++ src/main/java/com/daveproject/todo/config/SpringSecurityConfig.java
@@ -7,11 +7,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -23,21 +30,22 @@ public class SpringSecurityConfig {
         return http.build();
     }
 
+    @Bean
     public UserDetailsService userDetailsService(){
 
-        UserDetails ramesh = User.builder()
+        UserDetails dave = User.builder()
                 .username("dave")
-                .password("password")
+                .password(passwordEncoder().encode("password"))
                 .roles("USER")
                 .build();
 
         UserDetails admin = User.builder()
                 .username("admin")
-                .password("admin")
+                .password(passwordEncoder().encode("admin"))
                 .roles("ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(ramesh, admin);
+        return new InMemoryUserDetailsManager(dave, admin);
     }
 
 }
